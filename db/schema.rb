01af1223b4cf727157bd20f1a33b6b56779d8789
antoine ayoub <antoine.ayoub@gmail.com>
2016-02-15 16:14:15 +0100

    add fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215125837) do
+ActiveRecord::Schema.define(version: 20160215151235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20160215125837) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "summary"
+    t.string   "user_picture"
+    t.string   "phone_number"
+    t.date     "birth_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
