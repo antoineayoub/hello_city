70de0df849bad15f606f5f33ac499aa49afe471c
antoine ayoub <antoine.ayoub@gmail.com>
2016-02-17 11:18:13 +0100

    fixed logo for production

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
-      #user.user_picture = auth.info.image  #Cloudinary::Uploader.upload(auth.info.image)["public_id"]
+      Cloudinary::Uploader.upload(auth.info.image) #user.user_picture = auth.info.image
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
     end
