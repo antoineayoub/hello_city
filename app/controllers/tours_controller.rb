3d2ed9fa3c735310a975c2e72d5ffe5f8027b53e
Adrien de Villoutreys <adrien.de.villoutreys@gmail.com>
2016-02-19 14:29:01 +0100

    adv all good (gem installed for search

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,14 +2,43 @@ class ToursController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-  search
+    search
     @markers = Gmaps4rails.build_markers(@tours) do |tour, marker|
       marker.lat tour.latitude
       marker.lng tour.longitude
     end
     @skip_footer = true
-    @tours = @tours.order(:price).page
-  end
+
+    params_hash = {
+      guide_level: params[:guide_level],
+      price: params[:price],
+      provides_car: params[:provides_car],
+      provides_ticket: params[:provides_ticket],
+      provides_food: params[:provides_food],
+      tour_duration: params[:tour_duration],
+      language: params[:language]
+    }
+    search_query = ""
+
+    if  params[:tour_duration].to_i != 0
+      search_query << "(tour_duration <= #{params[:tour_duration].to_i})"
+    end
+
+    if  params[:price].to_i != 0
+      search_query << "(price <= #{params[:price].to_i})"
+    end
+
+    params_hash.each do |key, value|
+      search_query << " AND (#{key.to_s} = #{params_hash[key]}) " unless params_hash[key] = "" || params_hash[key] = nil
+    end
+
+    if  Tour.search_for(search_query).count >= 0
+      @tours = Tour.search_for(search_query)
+    else
+      @tours = Tour.all
+    end
+end
+
 
   def index_user
     @user_tours = Tour.where("user_id = #{current_user.id}")
@@ -17,10 +46,6 @@ class ToursController < ApplicationController
 
   def new
     @tour = Tour.new
-  end
-
-  def getMarker
-    i
   end
 
   def create
@@ -63,7 +88,6 @@ class ToursController < ApplicationController
       marker.lat tour.latitude
       marker.lng tour.longitude
     end
-    count_pending
   end
 
   def search
@@ -79,5 +103,4 @@ class ToursController < ApplicationController
   def tour_params
     params.require(:tour).permit(:name, :description, :live, :guide_level, :language, :address, :price, photos: [])
   end
-
 end
