af100682f53438d4c96a063aad39f14d4e5bf1c0
antoine ayoub <antoine.ayoub@gmail.com>
2016-02-21 20:41:21 +0100

    FB solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
-      user.picture_url = facebook_profile_image_tag(auth.info.image,:transformation=>[
-                        {:radius=>"max", :width=>400, :height=>400, :crop=>:crop, :gravity=>:face},
-                        {:width=>200}
-                        ])
+      user.picture_url = auth.info.image
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
     end
